d6b72f99da1581282284cf3f48b070b0d435a9ca
AsiburSumon <sumonasif68@gmail.com>
2022-10-27 16:44:56 +0600

    server done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./Data/courses.json');
+const course = require('./Data/course.json')
 
 app.get('/', (req, res) =>{
     res.send('API is running');
@@ -13,6 +14,16 @@ app.get('/', (req, res) =>{
 
 app.get ('/courses', (req, res)=>{
     res.send(courses)
+})
+
+app.get('/allcourses',(req, res)=>{
+    res.send(course);
+})
+
+app.get('/allcourses/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n.id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, ()=> {
