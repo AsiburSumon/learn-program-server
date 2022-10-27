f5fd990a09c9390f6ac4dfd7de26c79d9b4a5113
AsiburSumon <sumonasif68@gmail.com>
2022-10-27 08:23:47 +0600

    courses data added

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./Data/courses.json');
+
 app.get('/', (req, res) =>{
     res.send('API is running');
 });
+
+app.get ('/courses', (req, res)=>{
+    res.send(courses)
+})
 
 app.listen(port, ()=> {
     console.log('server is running for assignment on', port);
